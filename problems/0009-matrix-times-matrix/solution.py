d44d1fd9d61614f6c7c323259f2a12dc3a057808
BetterThanYou73 <94681550+BetterThanYou73@users.noreply.github.com>
2026-09-21 03:16:44 +0000

    Update: Matrix times Matrix

--- problems/0009-matrix-times-matrix/solution.py
+++ problems/0009-matrix-times-matrix/solution.py
@@ -1,14 +1,15 @@
-import torch
+import numpy as np
 
-def matrixmul(a, b) -> torch.Tensor:
-    """
-    Multiply two matrices using PyTorch.
-    Inputs can be Python lists, NumPy arrays, or torch Tensors.
-    Returns a 2D tensor of shape (m, n) or a scalar tensor -1 if dimensions mismatch.
-    """
-    val = -1
+def matrixmul(a:list[list[int|float]],
+              b:list[list[int|float]])-> list[list[int|float]]:
 
-    if len(a[0]) == len(b):
-        val = a @ b
 
-    return val
+    col = len(a[0])
+    row = len(b)
+
+    c = -1
+
+    if col == row:   
+        c = np.matmul(a, b)
+        
+    return c
