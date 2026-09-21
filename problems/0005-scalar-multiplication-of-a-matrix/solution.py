a048969c5d5a7ba6b1d9c3eebc5533a4c09c19f5
BetterThanYou73 <94681550+BetterThanYou73@users.noreply.github.com>
2026-09-21 03:11:26 +0000

    Update: Scalar Multiplication of a Matrix

--- problems/0005-scalar-multiplication-of-a-matrix/solution.py
+++ problems/0005-scalar-multiplication-of-a-matrix/solution.py
@@ -1,12 +1,6 @@
-import torch
+import numpy as np
 
-def scalar_multiply(matrix, scalar) -> torch.Tensor:
-    """
-    Multiply each element of a 2D matrix by a scalar using PyTorch.
-    Inputs can be Python lists, NumPy arrays, or torch Tensors.
-    Returns a 2D tensor of the same shape.
-    """
-    # Convert input to tensor
-    m_t = torch.as_tensor(matrix, dtype=torch.float)
-    # Your implementation here
-    return m_t * scalar
+
+def scalar_multiply(matrix: list[list[int|float]], scalar: int|float) -> list[list[int|float]]:
+	# Your code here
+	return np.multiply(matrix, scalar)
