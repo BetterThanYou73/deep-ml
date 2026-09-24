69160dc355a381e5c38cff4383a2cb602de0e0b0
BetterThanYou73 <94681550+BetterThanYou73@users.noreply.github.com>
2026-09-24 21:31:35 +0000

    Solve: Expected Value and Variance of an n-Sided Die

---
+++ problems/0179-expected-value-and-variance-of-an-n-sided-die/solution.py
@@ -0,0 +1,16 @@
+def dice_statistics(n: int) -> tuple[float, float]:
+	"""
+	Compute the expected value and variance of a fair n-sided die roll.
+
+	Args:
+		n (int): Number of sides of the die
+
+	Returns:
+		tuple: (expected_value, variance)
+	"""
+	# Your code here
+	expected = (n+1)/2
+	var = (n**2 - 1)/12
+
+	return (expected, var)
+
